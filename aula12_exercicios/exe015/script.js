d2368f2f41518e777c747a5fcb3182dfef81d217
Cecília <cecbrennand@gmail.com>
2026-01-09 21:11:45 -0300

    html e css do exercicio feitos

---
+++ aula12_exercicios/exe015/script.js
@@ -0,0 +1,9 @@
+function verificar() {
+    var ano = document.getElementById('iano')
+    var sexo = document.getElementsByName('sexo')
+    var img = document.getElementById('imagem')
+
+    switch(img) {
+        ca
+    }
+}
