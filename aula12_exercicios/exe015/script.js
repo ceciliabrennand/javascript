fa0963069b80edaddc2ca186f4a5ba473e7d9c91
Cecília <cecbrennand@gmail.com>
2026-01-09 21:40:24 -0300

    parte do script feito

--- aula12_exercicios/exe015/script.js
+++ aula12_exercicios/exe015/script.js
@@ -1,9 +1,18 @@
 function verificar() {
-    var ano = document.getElementById('iano')
+    var data = new Date()
+    var ano = data.getFullYear()
+    var anas = document.getElementById('iano')
     var sexo = document.getElementsByName('sexo')
     var img = document.getElementById('imagem')
+    var txt = document.getElementById('texto')
 
-    switch(img) {
-        ca
+    if (Number(iano.value) < 1925 || Number(iano.value) > 2025) {
+        window.alert('[ERRO] Complete o formulário corretamente') 
+    } else if ( ano - Number(iano.value) < 5) {
+        var idade = ano - Number(iano.value)
+        txt.innerHTML = `Foi detectado um bebê de ${idade} anos.`
+        img.style.display = 'block'
+    } else if (ano - Number(iano.value) >= 5 && Number(iano.value) < 14) {
+        
     }
 }
