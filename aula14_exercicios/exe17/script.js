f7565603588ee36ff426f4daa6befa71f9cffc05
Cecília <cecbrennand@gmail.com>
2026-01-13 16:45:00 -0300

    melhorei o js

--- aula14_exercicios/exe17/script.js
+++ aula14_exercicios/exe17/script.js
@@ -7,10 +7,11 @@ function clicar() {
     } else {
         var num = Number(numero.value)
         var c = 1
-
+        tab.innerHTML = ''
         while (c <= 10) {
             var item = document.createElement('option')
             item.text = `${num} x ${c} = ${num * c}`
+            item.value = `tab${c}`
             tab.appendChild(item)
             c++
         }
