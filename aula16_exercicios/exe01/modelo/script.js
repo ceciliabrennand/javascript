a5f7419ceccfacc95cac601e6622325779380a7f
Cecília <cecbrennand@gmail.com>
2026-01-18 19:22:35 -0300

    css e html do desafio feitos

---
+++ aula16_exercicios/exe01/modelo/script.js
@@ -0,0 +1,9 @@
+function clicar () {
+    let numero = document.getElementById(Number('fnum'))
+    let numb = [numero, ]
+    let num = function adicionar() {
+        numb.push(numero)
+    }
+
+
+}
