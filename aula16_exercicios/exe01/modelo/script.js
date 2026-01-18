001ff74ea89fddc882ff4b8b8656581396242e5d
Cecília <cecbrennand@gmail.com>
2026-01-18 19:50:45 -0300

    primeira parte do js completa

--- aula16_exercicios/exe01/modelo/script.js
+++ aula16_exercicios/exe01/modelo/script.js
@@ -1,9 +1,45 @@
-function clicar () {
-    let numero = document.getElementById(Number('fnum'))
-    let numb = [numero, ]
-    let num = function adicionar() {
-        numb.push(numero)
+let num = document.getElementById('fnum')
+let lista = document.querySelector('select#flista')
+let res = document.querySelector('div#res')
+let valores = []
+
+function isNumero(n) {
+    if(Number(n) >= 1 && Number(n) <= 100) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function inLista(n, l) {
+    if(l.indexOf(Number(n)) != -1) {
+        return true 
+    } else {
+        return false
+    }
+}
+
+function adicionar () {
+    if(isNumero(num.value) && !inLista(num.value, valores)) {
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
+    } else {
+        window.alert('Valor inválido ou já encontrado na lista.')
     }
 
+    num.value = ''
+    num.focus()
+}
 
+function finalizar () {
+    if (valores.length == 0) {
+        window.alert('Adicione valores antes de finalizar.')
+    } else {
+        let tot = valores.length
+        res.innerHTML = ''
+        res.innerHTML += `<p> Ao todo, temos ${tot} números cadastrados.</p>`
+    }
 }
